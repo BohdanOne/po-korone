da4b90dacdf7703d615aa7cd393387fe5ff10d6c
Bohdan <bohdanimiela@gmail.com>
2024-04-09 20:29:54 +0200

    feat: prepare for deploying on cloudflare pages

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,7 @@
 import type { Peak } from '$lib/types';
 
+export const prerender = true;
+
 export async function load({ fetch }) {
 	const response = await fetch('/api/peaks');
 	const peaks: Peak[] = await response.json();
