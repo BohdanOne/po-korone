1b21b0e4b787bd67cbef3d45ebf026684ce2df9a
Bohdan <bohdanimiela@gmail.com>
2024-04-09 19:44:19 +0200

    feat: move page navigation to separate component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,59 +1,13 @@
 <script lang="ts">
 	import '../styles.css';
+	import PageNavigation from '$lib/components/PageNavigation.svelte';
 
 	export let data;
-
-	let peaksListOpen = false;
-
-	function handlePeaksListToggle() {
-		peaksListOpen = !peaksListOpen;
-	}
-
-	function handlePeaksListClose() {
-		peaksListOpen = false;
-	}
 </script>
 
-<svelte:body on:click={handlePeaksListClose} />
 <div class="flex flex-col h-dvh">
 	<header class="p-4">
-		<nav>
-			<ul class="flex justify-between md:flex-col md:gap-2">
-				<li class="md:w-max">
-					<h1>
-						<a href="/">Po koronę</a>
-					</h1>
-				</li>
-
-				<li>
-					<button
-						class="md:hidden"
-						id="peaksListToggle"
-						aria-expanded={peaksListOpen}
-						aria-controls="peaksList"
-						on:click|stopPropagation={handlePeaksListToggle}
-					>
-						Szczyty
-					</button>
-					<ul
-						class={`${
-							peaksListOpen
-								? 'absolute px-4 pt-2 pb-4 right-0 ring-1 max-h-[80svh] overflow-scroll bg-white'
-								: 'hidden'
-						} 
-					md:relative md:flex md:flex-wrap md:gap-2 md:px-0 md:ring-0`}
-						id="peaksList"
-						aria-label="Lista Szczytów"
-					>
-						{#each data.peaks as peak}
-							<li class="text-right md:text-left">
-								<a class="" href={`/${peak.slug}`}>{peak.name}</a>
-							</li>
-						{/each}
-					</ul>
-				</li>
-			</ul>
-		</nav>
+		<PageNavigation peaks={data.peaks} />
 	</header>
 	<main class="p-4 grow">
 		<slot />
